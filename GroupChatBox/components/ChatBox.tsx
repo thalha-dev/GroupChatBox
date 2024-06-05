7fbb92a39c1a8ed00215cc55d7242928ead3b44a
thalha-dev <dev.thalha@gmail.com>
2024-06-05 14:53:13 +0530

    add fields to customize theme

--- GroupChatBox/components/ChatBox.tsx
+++ GroupChatBox/components/ChatBox.tsx
@@ -16,6 +16,28 @@ export type ChatBoxProp = {
   dateColumnName: string | null;
   userName: string | null;
   userNameColumnName: string | null;
+  controlWidth: string | null;
+  controlHeight: string | null;
+  messageBoxBackgroundColor: string | null;
+  messageBoxBorderColor: string | null;
+  userMessageBackgroundColor: string | null;
+  currentUserMessageBackgroundColor: string | null;
+  userMessageColor: string | null;
+  currentUserMessageColor: string | null;
+  messageFontSize: string | null;
+  messageMetaDataFontSize: string | null;
+  userMetaMessageBackgroundColor: string | null;
+  currentUserMetaMessageBackgroundColor: string | null;
+  userMetaMessageColor: string | null;
+  currentUserMetaMessageColor: string | null;
+  userMessageBorderColor: string | null;
+  currentUserMessageBorderColor: string | null;
+  inputBackgroundColor: string | null;
+  inputColor: string | null;
+  inputBorderColor: string | null;
+  sendButtonBackgroundColor: string | null;
+  sendButtonColor: string | null;
+  sendButtonBorderColor: string | null;
 };
 
 export const ChatBox: React.FC<ChatBoxProp> = React.memo(
@@ -30,6 +52,27 @@ export const ChatBox: React.FC<ChatBoxProp> = React.memo(
     timeZone,
     userNameColumnName,
     userName,
+    controlWidth,
+    controlHeight,
+    messageBoxBackgroundColor,
+    userMessageBackgroundColor,
+    currentUserMessageBackgroundColor,
+    userMessageColor,
+    currentUserMessageColor,
+    messageFontSize,
+    messageMetaDataFontSize,
+    userMetaMessageBackgroundColor,
+    currentUserMetaMessageBackgroundColor,
+    userMetaMessageColor,
+    currentUserMetaMessageColor,
+    userMessageBorderColor,
+    currentUserMessageBorderColor,
+    inputBackgroundColor,
+    inputColor,
+    inputBorderColor,
+    sendButtonBackgroundColor,
+    sendButtonColor,
+    sendButtonBorderColor,
   }: ChatBoxProp): React.JSX.Element => {
     const [message, setMessage] = React.useState("");
     const messageContainerRef = React.useRef<HTMLDivElement>(null);
@@ -86,6 +129,22 @@ export const ChatBox: React.FC<ChatBoxProp> = React.memo(
             currentUserEmail={userEmail}
             userEmailInRecord={userEmailInRecord}
             chatMessage={chatMessage}
+            userMessageBackgroundColor={userMessageBackgroundColor}
+            currentUserMessageBackgroundColor={
+              currentUserMessageBackgroundColor
+            }
+            userMessageColor={userMessageColor}
+            currentUserMessageColor={currentUserMessageColor}
+            messageFontSize={messageFontSize}
+            messageMetaDataFontSize={messageMetaDataFontSize}
+            userMetaMessageBackgroundColor={userMetaMessageBackgroundColor}
+            currentUserMetaMessageBackgroundColor={
+              currentUserMetaMessageBackgroundColor
+            }
+            userMetaMessageColor={userMetaMessageColor}
+            currentUserMetaMessageColor={currentUserMetaMessageColor}
+            userMessageBorderColor={userMessageBorderColor}
+            currentUserMessageBorderColor={currentUserMessageBorderColor}
           />
         );
       });
@@ -135,8 +194,21 @@ export const ChatBox: React.FC<ChatBoxProp> = React.memo(
     };
 
     return (
-      <div className="message-app-container">
-        <div className="all-message-container" ref={messageContainerRef}>
+      <div
+        className="message-app-container"
+        style={{
+          width: controlWidth as string,
+          height: controlHeight as string,
+        }}
+      >
+        <div
+          className="all-message-container"
+          ref={messageContainerRef}
+          style={{
+            backgroundColor: messageBoxBackgroundColor as string,
+            borderColor: userMessageBackgroundColor as string,
+          }}
+        >
           {chatMessageArray ? chatMessageArray : ""}
         </div>
 
@@ -148,8 +220,22 @@ export const ChatBox: React.FC<ChatBoxProp> = React.memo(
             value={message}
             onChange={handleMessageChange}
             onKeyUp={handleKeyUp}
+            style={{
+              backgroundColor: inputBackgroundColor as string,
+              color: inputColor as string,
+              border: `2px solid ${inputBorderColor as string}`,
+              fontSize: messageFontSize as string,
+            }}
           />
-          <button className="message-send-button" onClick={handleButtonClick}>
+          <button
+            className="message-send-button"
+            onClick={handleButtonClick}
+            style={{
+              backgroundColor: sendButtonBackgroundColor as string,
+              color: sendButtonColor as string,
+              border: `2px solid ${sendButtonBorderColor as string}`,
+            }}
+          >
             <IoMdArrowRoundUp />
           </button>
         </div>
--- GroupChatBox/components/Message.tsx
+++ GroupChatBox/components/Message.tsx
@@ -7,6 +7,18 @@ export type MessageProp = {
   createdDateTime: string | null;
   chatMessage: string | null;
   recordId: string | null;
+  userMessageBackgroundColor: string | null;
+  currentUserMessageBackgroundColor: string | null;
+  userMessageColor: string | null;
+  currentUserMessageColor: string | null;
+  messageFontSize: string | null;
+  messageMetaDataFontSize: string | null;
+  userMetaMessageBackgroundColor: string | null;
+  currentUserMetaMessageBackgroundColor: string | null;
+  userMetaMessageColor: string | null;
+  currentUserMetaMessageColor: string | null;
+  userMessageBorderColor: string | null;
+  currentUserMessageBorderColor: string | null;
 };
 
 const Message = ({
@@ -16,22 +28,78 @@ const Message = ({
   createdDateTime,
   recordId,
   chatMessage,
+  userMessageBackgroundColor,
+  currentUserMessageBackgroundColor,
+  userMessageColor,
+  currentUserMessageColor,
+  messageFontSize,
+  messageMetaDataFontSize,
+  userMetaMessageBackgroundColor,
+  currentUserMetaMessageBackgroundColor,
+  userMetaMessageColor,
+  currentUserMetaMessageColor,
+  userMessageBorderColor,
+  currentUserMessageBorderColor,
 }: MessageProp) => {
   const isMessageBelongsToCurrentUser = userEmailInRecord === currentUserEmail;
 
   return (
     <div
       className={`message-container ${isMessageBelongsToCurrentUser ? "current-user-message-container" : ""}`}
+      style={{
+        backgroundColor: isMessageBelongsToCurrentUser
+          ? (currentUserMessageBackgroundColor as string)
+          : (userMessageBackgroundColor as string),
+        color: isMessageBelongsToCurrentUser
+          ? (currentUserMessageColor as string)
+          : (userMessageColor as string),
+        border: `2px solid ${
+          isMessageBelongsToCurrentUser
+            ? (currentUserMessageBorderColor as string)
+            : (userMessageBorderColor as string)
+        }`,
+      }}
       key={recordId}
     >
-      <div className="message-content">{chatMessage}</div>
-      <div className="message-username-datetime-container">
+      <div
+        className="message-content"
+        style={{
+          fontSize: messageFontSize as string,
+        }}
+      >
+        {chatMessage}
+      </div>
+      <div
+        className="message-username-datetime-container"
+        style={{
+          backgroundColor: isMessageBelongsToCurrentUser
+            ? (currentUserMetaMessageBackgroundColor as string)
+            : (userMetaMessageBackgroundColor as string),
+          color: isMessageBelongsToCurrentUser
+            ? (currentUserMetaMessageColor as string)
+            : (userMetaMessageColor as string),
+        }}
+      >
         {!isMessageBelongsToCurrentUser ? (
-          <div className="message-useranme">{userNameInRecord}</div>
+          <div
+            className="message-useranme"
+            style={{
+              fontSize: messageMetaDataFontSize as string,
+            }}
+          >
+            {userNameInRecord}
+          </div>
         ) : (
           ""
         )}
-        <div className="message-datetime">{createdDateTime}</div>
+        <div
+          className="message-datetime"
+          style={{
+            fontSize: messageMetaDataFontSize as string,
+          }}
+        >
+          {createdDateTime}
+        </div>
       </div>
     </div>
   );
